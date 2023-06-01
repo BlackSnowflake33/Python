b987d46f12d7010eff578cdd7e3d2141b6fd3c85
Катя <email@yandex.ru>
2023-06-01 14:19:36 +0300

    List, Tuple

---
+++ Python/Lesson/2/1.py
@@ -0,0 +1,11 @@
+# Создать список, состоящий из четных чисел в диапазоне от 1 до 100.
+
+list_1= []
+print(list_1)
+
+for i in range (1, 101):
+    list_1.append(i)
+print(list_1)
+
+list_2=[i for i in range(-100, 1)]
+print(list_2)
